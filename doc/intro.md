0a4dd6794d730f695439fd9874f0ca28454816fd
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2014-09-04 22:31:47 +0200

    Lein generation

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hipchat-github-bot
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject hipchat-github-bot "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main ^:skip-aot hipchat-github-bot.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
