af12bc5660cc3832da710fadd1acbe2b9754a2d3
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2014-09-06 16:02:35 +0200

    write in the json file the info so the server can be turned on/off without issue

--- src/isaac/handler.clj
+++ src/isaac/handler.clj
@@ -9,29 +9,28 @@
    [isaac.hipchat :as hipchat])
   (:gen-class))
 
-(declare info)
-(def ^{:private true} basic-info (json/read-str
-                                  (slurp (io/file (-> (java.io.File. "info.json") .getAbsolutePath)))
-                                  :key-fn keyword))
+(def ^{:private true} info-path (io/file (-> (java.io.File. "info.json") .getAbsolutePath)))
+
+(def ^{:private true} info (atom (json/read-str
+                                  (slurp info-path)
+                                  :key-fn keyword)))
 
 ;;
 ;; CORE
 ;;
 
 (defroutes app-routes
-  (GET "/" {} (hipchat/capabilities-descriptor basic-info))
+  (GET "/" {} (hipchat/capabilities-descriptor @info))
   (POST "/" {headers :headers body :body}
         (do
           (if (= (get headers "user-agent") "HipChat.com")
             (let [new-info (hipchat/retrieve-token headers (json/read-str (slurp body) :key-fn keyword))
                   token (:token new-info)
                   room-id (:room-id new-info)]
-              (def ^{:private true} info
-                {:token token
-                 :room-id room-id
-                 :url (:url basic-info)})
+              (swap! info assoc :token token :room-id room-id)
+              (spit info-path (json/write-str @info))
               "200")
-            (github/handle-post headers (json/read-str (slurp body) :key-fn keyword) info))
+            (github/handle-post headers (json/read-str (slurp body) :key-fn keyword) @info))
           "200"))
   (route/not-found ""))
 
